fcbb8217ca52eb9ee6966ff967e8d889ff543d12
Matthieu Faugere <matthieu.faugere@lifen.fr>
2019-12-13 09:27:48 +0100

    setup lib

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const {Button} = window.reactSampleComponentsLibrary;
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button text="react" onClick={() => console.log('plop')}></Button>
       </header>
     </div>
   );
